a691cb3cfc3837ca2b5961d2369e4a4b01cc88e9
seungwon0113 <dhkd0113@naver.com>
2024-10-11 16:31:32 +0900

    chapter_03 업로드

--- chapter_02/config/urls.py
+++ chapter_02/config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from users import views as users_views
 from todo.views import todo_list, todo_info  # 추가
 
 urlpatterns = [
@@ -25,4 +25,9 @@ urlpatterns = [
 
     path('todo/', todo_list, name='todo_list'),  # 추가
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),  # 추가
+
+    # user login
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', users_views.sign_up, name='sign_up'),
+    path('accounts/login/', users_views.login, name='login'),
 ]
